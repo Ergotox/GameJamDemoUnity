2969b88be3e7a245ea59aa61aefa65ce8304640a
Diego Santivañez <Diegojsf17@gmail.com>
2023-10-15 12:17:34 -0500

    feat: change sprites added

--- Assets/Scripts/Villain.cs
+++ Assets/Scripts/Villain.cs
@@ -1,18 +1,25 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Villain : MonoBehaviour
 {
+    private int health = 100;
     // Start is called before the first frame update
-    void Start()
+    
+  
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        
+        if(other.CompareTag("Note"))
+        {
+            health -= 10;
+            print(health);
+            if(health <= 0)
+            {
+                Destroy(gameObject);
+            }
+        }
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   
 }
